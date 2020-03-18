d8da8c4775f99cace3cec7d00b43ba978e0508f5
nbawsum360 <44253401+nbawsum360@users.noreply.github.com>
2020-03-18 14:14:45 -0400

    Add files via upload

---
+++ Lab01/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private const string V = "Hello World!";
+        private const string V1 = " My Name is Nicholas Lemke!";
+       
+
+        static void Main(string[] args)
+        {
+            string V100 = V1;
+            string V50 = V;
+            string str = $"{V}{V1} ";
+                System.Console.WriteLine(str);
+           System.Console.ReadKey();
+        }
+    }
+}
